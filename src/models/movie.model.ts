cd919009b198ac5a2f88ae486003ef1dfe5f0e65
marijavuleticc <marijavuletic002@gmail.com>
2025-03-18 11:57:17 +0100

    Add models

---
+++ src/models/movie.model.ts
@@ -0,0 +1,13 @@
+export interface MovieModel{
+    id: 7,
+	name: string
+	genre: string
+	duration: string
+	director: string,
+	actors: string,
+	releaseDate: string
+	ticketPrice: string
+	averageRating: number
+	poster: string
+	description:
+}
