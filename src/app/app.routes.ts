2932eaeba2ae200a6ce03bf32455561d5647cf60
marijavuleticc <marijavuletic002@gmail.com>
2025-03-20 10:39:01 +0100

    User service and signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'search', component: SearchComponent},
+    {path: 'signup', component: SignupComponent},
     {path:'**', redirectTo: ''}
+    
 ];
---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  imports: [MatInputModule, MatCardModule,RouterLink, MatButtonModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
