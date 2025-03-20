d6b3ecfaa0cf5b496400de7ce1b3acf041a19745
marijavuleticc <marijavuletic002@gmail.com>
2025-03-20 10:54:50 +0100

    Added login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SearchComponent } from './search/search.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
     {path: 'search', component: SearchComponent},
     {path: 'signup', component: SignupComponent},
+    {path: 'login', component: LoginComponent},
     {path:'**', redirectTo: ''}
     
 ];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatInputModule,MatButtonModule,MatCardModule,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
